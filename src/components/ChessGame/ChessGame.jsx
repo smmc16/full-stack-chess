61b046c34ae4dda5d50158ba2128d98466cab799
Sarah McCartney <smmc16@gmail.com>
2024-05-25 14:46:28 -0500

    added onDrop and makeMove functions to ChessGame

--- src/components/ChessGame/ChessGame.jsx
+++ src/components/ChessGame/ChessGame.jsx
@@ -5,7 +5,35 @@ import { useSelector, useDispatch } from 'react-redux';
 import './ChessGame.css'
 
 function ChessGame() {
+  const [position, setPosition] = useState('start');
+  const [game, setGame] = useState(new Chess());
+  const [turn, setTurn] = useState(game.turn());
   
+  const makeMove = (move) => {
+      game.move(move);
+      setGame(new Chess(game.fen()));
+      setTurn(game.turn());
+    }
+
+  function onDrop(sourceSquare, targetSquare) {
+      const movePiece = makeMove({
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: "q",
+      });
+      const move = {
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: "q", 
+      };
+      setPosition(game.fen());
+        
+      if (movePiece === null) {return false}
+      else { 
+          socket.emit('makeMove', move, roomID);
+      };
+
+    }
   
   return (
     <div>
