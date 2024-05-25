8a8ca02d3dde1a86b91b5eefaaf3b4fd930ee585
Sarah McCartney <smmc16@gmail.com>
2024-05-24 22:35:28 -0500

    added packages and update sql file

--- database.sql
+++ database.sql
@@ -7,3 +7,15 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "games" (
+	"id" SERIAL PRIMARY KEY,
+	"created" DATE DEFAULT CURRENT_DATE,
+	"in_progress" BOOLEAN DEFAULT TRUE,
+	"room_id" VARCHAR (80),
+	"white" INT REFERENCES "user",
+	"black" INT REFERENCES "user",
+	"position" VARCHAR(1000) DEFAULT 'start',
+	"winner" INT REFERENCES "user"
+	);
+	
