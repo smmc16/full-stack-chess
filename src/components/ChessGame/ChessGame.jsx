a89e6e3e48c5258f30f027653c3b99f57648d938
Sarah McCartney <smmc16@gmail.com>
2024-05-25 14:39:45 -0500

    added imports to ChessGame

---
+++ src/components/ChessGame/ChessGame.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+import { Chess } from "chess.js";
+import { Chessboard } from "react-chessboard";
+import { useSelector, useDispatch } from 'react-redux';
+import './ChessGame.css'
+
+function ChessGame() {
+  
+  
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default TemplateFunction;
